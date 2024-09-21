e4ab4cf9ffb792651f463157f7b76b07ea8667d3
Grm <jeremylombard.mail@gmail.com>
2024-09-22 00:17:34 +0200

    Creating Vert & Frag

---
+++ compile.bat
@@ -0,0 +1,4 @@
+﻿"C:\VulkanSDK\1.3.290.0\Bin\glslc.exe" "Shaders\simple_shader.vert" -o "Shaders\simple_shader.vert.spv"
+C:\VulkanSDK\1.3.290.0\Bin\glslc.exe Shaders\simple_shader.frag -o Shaders\simple_shader.frag.spv
+C:\VulkanSDK\1.3.290.0\Bin\glslc.exe Shaders\simple_shader.vert -o Shaders\simple_shader.vert.spv
+pause
